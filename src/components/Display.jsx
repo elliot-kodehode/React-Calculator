ebe844fbc617aa2ae8a1e017f5ba1fc1ed25974c
Elliot <elliot.kodehode@gmail.com>
2024-03-30 23:36:30 +0100

    button layout and display

---
+++ src/components/Display.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Display() {
+    return (
+        <div className={Styles.display}>
+            <p className={styles.currentCalc}></p>
+            <p className={styles.currentResult}></p>
+        </div>
+    )
+}
