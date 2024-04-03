1d2ddbb38f21188533ec59c86f6c92021a553897
Elliot <elliot.kodehode@gmail.com>
2024-04-03 13:04:31 +0200

    Trying to fix a problem

--- src/components/CalcWrapper.jsx
+++ src/components/CalcWrapper.jsx
@@ -20,46 +20,38 @@ import {
 export default function Calc() {
   const [currentCalc, setCurrentCalc] = useState(``);
   const [result, setResult] = useState(``);
-  const [currentNum, setCurrentNum] = useState(0);
   const operators = ["+", "-", "/", "=", "×"];
 
   const handleChange = (value) => {
+    
     if (value === "RESET") {
       setCurrentCalc("");
-      setResult(currentCalc);
+      setResult("");
+      
     } else if (value === "DEL") {
-      // Removes last character of the equation after trimming whitespace, and then trims whitespace after removing the character
-      setCurrentCalc(currentCalc.trim().slice(0, -1).trim());
-      //Checks if the last char is now a number or an operator and removes the current expected result if it is an operator
-      operators.includes(currentCalc.trim().slice(0, -1))
-        ? setResult(``)
-        : setResult(evaluate(currentCalc.trim().slice(-1).trim()));
+      setCurrentCalc(currentCalc.slice(0, -1));
+      if (operators.includes(currentCalc.charAt(currentCalc.length - 1))) {
+        setResult("")
+      } else {setResult(evaluate(currentCalc.slice(0, -1)))}
+      // setResult(operators.includes(currentCalc.charAt(currentCalc.length)) ? `` : evaluate(currentCalc.slice(0, -1)));
+      console.log(currentCalc.slice(0, -1));
+
     } else {
       // Changes result to blank if an operator is added, and adds it with spacing to the calc
       if (
-        operators.includes(value) &&
-        !operators.includes(currentCalc.trim().slice(-1))
+        operators.includes(value) 
+        // && !operators.includes(currentCalc.slice(-1)) 
+        //to fix when i have space betweeen operators later
       ) {
-        setCurrentCalc(currentCalc + " " + value + " ");
+        setCurrentCalc(currentCalc + value);
         setResult("");
+
       } else {
         setCurrentCalc(currentCalc + value);
         setResult(evaluate(currentCalc + value));
       }
     }
-    // value === "RESET"
-    //   ? (setCurrentCalc(``), setResult(currentCalc))
-    //   : value === "DEL"
-    //   ? (setCurrentCalc(currentCalc.trim().slice(0, -1).trim()), setResult(``))
-    //   : // Changes result to blank if an operator is added, and adds it with spacing to the calc
-    //   operators.includes(value) &&
-    //     !operators.includes(currentCalc.trim().slice(0, -1))
-    //   ? (setCurrentCalc(currentCalc + " " + value + " "), setResult(""))
-    //   : (setCurrentCalc(currentCalc + value),
-    //     setResult(evaluate(currentCalc + value)));
-
-    // console.log(equation);
-  };
+};
 
   return (
     <>
